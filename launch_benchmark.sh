c308bd115c6e3434fced5bc78208bd451929808b
Bryant Curto <bryantcurto@gmail.com>
2020-11-30 20:16:50 -0500

    Improved benchmarking script

--- launch_benchmark.sh
+++ launch_benchmark.sh
@@ -2,8 +2,12 @@
 DEFAULT_MESSAGE_SIZE=$((2 ** 7))
 DEFAULT_THREADS=64
 DEFAULT_SLEEP_SEC=180
+SERVER_HOST=192.168.199.7
 
-MAX_SOCKETS=1000
+MAX_SOCKETS=3000
+#MAX_CLIENTS=2000
+MAX_CLIENTS=1000
+ulimit -n $MAX_SOCKETS
 
 if (( $# == 0 )) || (( $# > 4)); then
 	echo "USAGE: $0 OUTPUT_FILEPATH [MESSAGE_SIZE [NUM_THREADS [TEST_DURATION_SEC]]]"
@@ -14,17 +18,25 @@ if (( $# == 0 )) || (( $# > 4)); then
 	exit -1
 fi
 
+cleanup() {
+  for p in $pids; do
+    kill -9 $p
+  done
+}
+
+
 output_filename=$1
 message_size=${2:-$DEFAULT_MESSAGE_SIZE}
 num_threads=${3:-"$DEFAULT_THREADS"}
 sleep_sec=${4:-"$DEFAULT_SLEEP_SEC"}
 
-num_clients=$(echo "$MAX_SOCKETS / $num_threads" | bc -l | cut -d '.' -f 1)
-echo "Using $num_clients clients"
-./src/redis-benchmark -n 1000000000 -c $num_clients --threads $num_threads -t set -d $((2 ** 7)) 2>&1 | tee "$output_filename" &
-pid=$!
+echo "Using $MAX_CLIENTS clients"
+(./src/redis-benchmark -h $SERVER_HOST -n 1000000000 -c $MAX_CLIENTS --threads $num_threads -t set -d $message_size & echo $! >&3) 3>pid 2>&1 | tee "$output_filename" &
 
-echo "PID: $pid"
+pids="$(<pid) $!"
+echo "PIDs: $pids"
+
+trap cleanup 2 3 9 15
 sleep $sleep_sec
-kill -9 $pid
+cleanup
 
